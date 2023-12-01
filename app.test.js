c339767529528b7384efac82cf07ee59189a8176
TheVine-Dev <saulkb8@gmail.com>
2023-11-30 21:45:03 -0400

    confirms

--- app.test.js
+++ app.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Pruebas de la aplicación', () => {
-  it('Debería retornar ¡Hola, mundo! en la ruta raíz', async () => {
+  it('Debería retornar ¡Hola Saul Soto!! en la ruta raíz', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toBe('¡Hola, mundo!');
+    expect(response.text).toBe('¡Hola Saul Soto!!');
   });
 });
